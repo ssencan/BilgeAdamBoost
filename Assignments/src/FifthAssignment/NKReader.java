e5e60f283a1474781a90a817bb6fdd7c22d08751
Şükrü Şencan <s.sencan03@hotmail.com>
2023-10-15 22:49:16 +0300

    Added fifth assignment

--- Assignments/src/FifthAssignment/NKReader.java
+++ Assignments/src/FifthAssignment/NKReader.java
@@ -6,11 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class NKReader {
 	private String filePath;
 
